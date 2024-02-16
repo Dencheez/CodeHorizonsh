a4b2860b6a4f4634fa6d2e6e473c2fecf1f9ac11
Dancheez <foxden482@gmail.com>
2024-02-16 17:29:06 +0600

    html

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+
+    $(".modal-close").click(function () {
+        $(".modal-canvas").remove();
+    }); 
+    let onclick = document.getElementById('video-klip')
+    video.style.display = block;
